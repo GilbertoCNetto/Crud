4ada8d5caf74b8da119663082e2d1a9ded94c9bb
unknown <gilbertocnettoprofissional@gmail.comgit config --global user.email>
2023-09-21 02:58:14 -0300

    first commit

---
+++ Crud.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host='',  ##SEU HOST
+    user='',  #SEU USER
+    password='',  #SUA SENHA
+    database='',  #NOME DO SEU BANCO DE DADOS
+)
+cursor = conexao.cursor()
+
+'''PARA USAR CADA UMA DAS FUNÇÕes, DESCOMENTE O RESPECTIVO BLOCO'''
+# #CREATE
+# login = "pedro"
+# senha = "12345"
+# comando = f'INSERT INTO login(login, senha) VALUES ("{login}", "{senha}")'
+# cursor.execute(comando)
+# conexao.commit() #edita o banco de dados
+# resultado = cursor.fetchall() #ler o banco de dados
+
+
+# READ
+# comando = f'SELECT * FROM login'
+# cursor.execute(comando)
+# resultado = cursor.fetchall() #ler o banco de dados
+# print(resultado)
+
+# UPDATE
+# login = "pedro"
+# senha = "olamundo"
+# comando = f'UPDATE login SET senha = "{senha}" WHERE login = "{login}"'
+# cursor.execute(comando)
+# conexao.commit()
+
+# #DELETE
+# login = 'pedro'
+# comando = f'DELETE FROM login WHERE login = "{login}"'
+# cursor.execute(comando)
+# conexao.commit()
+
+cursor.close()
+conexao.close()
